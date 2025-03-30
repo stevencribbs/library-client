52797f39a32612c829f571b4e9f1d436cacf2a9c
Steven Cribbs <stevencribbs@msn.com>
2025-03-29 23:59:25 -0600

    added route links to home page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,11 +1,20 @@
 import React from 'react'
 import DateDisplay from '../components/DateDisplay'
+import { Link } from 'react-router-dom'
+import { ROUTES } from '../resources/routes-constants'
 
 const HomePage: React.FC = () => {
     return (
         <div style={{ position: 'relative', width: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column' }}>
             <h1 style={{ fontSize: '4em' }}>Personal Library Hub</h1>
             <DateDisplay />
+            <nav style={{ marginTop: '20px' }}>
+                <ul style={{ listStyleType: 'none', padding: 0 }}>
+                    <li><Link to={ROUTES.HOMEPAGE_ROUTE}>Home</Link></li>
+                    <li><Link to={ROUTES.BOOKSPAGE_ROUTE}>Books</Link></li>
+                    <li><Link to={ROUTES.QUOTESPAGE_ROUTE}>Quotes</Link></li>
+                </ul>
+            </nav>
         </div>
     )
 }
