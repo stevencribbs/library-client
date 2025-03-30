a5fac3e2d07525a6f25b1bc07843f442cc8062f4
Steven Cribbs <stevencribbs@msn.com>
2025-03-30 00:08:41 -0600

    added navigation buttons to home page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,22 +1,34 @@
 import React from 'react'
-import DateDisplay from '../components/DateDisplay'
-import { Link } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 import { ROUTES } from '../resources/routes-constants'
 
 const HomePage: React.FC = () => {
+    const navigate = useNavigate();
+
     return (
         <div style={{ position: 'relative', width: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column' }}>
             <h1 style={{ fontSize: '4em' }}>Personal Library Hub</h1>
-            <DateDisplay />
             <nav style={{ marginTop: '20px' }}>
                 <ul style={{ listStyleType: 'none', padding: 0 }}>
-                    <li><Link to={ROUTES.HOMEPAGE_ROUTE}>Home</Link></li>
-                    <li><Link to={ROUTES.BOOKSPAGE_ROUTE}>Books</Link></li>
-                    <li><Link to={ROUTES.QUOTESPAGE_ROUTE}>Quotes</Link></li>
+                    <li>
+                        <button onClick={() => navigate(ROUTES.HOMEPAGE_ROUTE)} style={{ cursor: 'pointer', margin: '10px 0' }}>
+                            Home
+                        </button>
+                    </li>
+                    <li>
+                        <button onClick={() => navigate(ROUTES.BOOKSPAGE_ROUTE)} style={{ cursor: 'pointer', margin: '10px 0' }}>
+                            Books
+                        </button>
+                    </li>
+                    <li>
+                        <button onClick={() => navigate(ROUTES.QUOTESPAGE_ROUTE)} style={{ cursor: 'pointer', margin: '10px 0' }}>
+                            Quotes
+                        </button>
+                    </li>
                 </ul>
             </nav>
         </div>
-    )
-}
+    );
+};
 
-export default HomePage
+export default HomePage;
